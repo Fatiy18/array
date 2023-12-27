af9ca9af2b34976c7ea18a22ea1738c3d2eac493
Fatiy18 <152783789+Fatiy18@users.noreply.github.com>
2023-12-27 14:35:26 +0400

    array

---
+++ main.js
@@ -0,0 +1,27 @@
+// task4
+
+// let arr1 = [2,3,4,5,6];
+// let arr2 = [3,4,5,7,8];
+
+// let x = arr1.filter(item => arr2.indexOf(item) == -1);
+
+// console.log(x);
+
+// task2
+
+// let num = prompt("eded daxil edin")
+
+// let tersnum = String(num).split("").reverse().join("");
+
+// console.log(tersnum);
+
+// task3
+
+function text(dog) {
+    let array = [];
+
+    for( let i = 0 , j = 1; x < dog.length; x++ , y++) {
+
+        array [x] = dog.slice(x,y);
+    }
+}
